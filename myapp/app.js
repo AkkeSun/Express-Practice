8e66287ac8db7bcb34305d73290e3ad37cd62680
sun <78401555+AkkeSun@users.noreply.github.com>
2021-08-18 17:42:54 +0900

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,3 +1,4 @@
+/*------------모듈 로드 ------------------*/
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,23 +6,48 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users'); 
+var fileRouter = require('./routes/file');
+var compression = require('compression');
+var session = require('express-session');
+var loginCheck = require('./my_modules/loginCheck').loginCheck;
 
+/*------------app 객채 생성 ----------------*/
 var app = express();
 
-// view engine setup
+/*------------view engine setup ------------*/
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
+/*------------미들웨어 로드 ----------------*/
 app.use(logger('dev'));
+// body-parser 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// 쿠키 설정
 app.use(cookieParser());
+// static 경로 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
+// jquery 경로 설정
+app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.use(compression());
+app.use(session({
+    secret:"sun-session", // 암호화
+    resave:false,
+    saveUninitialized:true  
+}));
+/*------------로그인 체크 -----------------*/
+app.use(function(req, res, next){
+  loginCheck(req,res,next);
+});
+
+/*------------라우팅 설정 -----------------*/
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/file', fileRouter);
 
+/*-------------- 에러 핸들러 ---------------*/
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
